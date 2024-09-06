f6afcd43fee37e2ef644a1d66b83ed01b0c2863b
MathisPFR <mathispetitfr@gmail.com>
2024-09-06 11:46:13 +0200

    more style in header add logo and style button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Importer useLocation
 import LogoutButton from './LogoutButton';
 
 const Header = ({ setIsAuthenticated }) => {
+  const location = useLocation(); // Utiliser useLocation pour savoir où l'on est
+
+  // Vérifier si le chemin est actif et appliquer les classes de style correspondantes
+  const isActive = (path) => location.pathname === path;
+
   return (
     <div>
       <div className="navbar bg-base-100">
         <div className="flex-1 gap-7">
+          {/* Logo */}
           <Link to="/" className="btn btn-ghost text-xl">
-            HOME
+            <img src="/logo-app.png" alt="Logo" className="w-12 h-12" />
           </Link>
+          
+          {/* Lien Transactions */}
           <div className="form-control">
             <div>
-              <Link to="/transactions">Liste des transactions</Link>
+              <Link
+                to="/transactions"
+                className={`btn btn-ghost ${isActive('/transactions') ? 'bg-blue-500 text-white' : ''}`}  // Appliquer le style actif
+              >
+                Liste des transactions
+              </Link>
             </div>
           </div>
+
+          {/* Lien Graphiques */}
           <div className="form-control">
             <div>
-              <Link to="/charts">Les graphiques</Link>
+              <Link
+                to="/charts"
+                className={`btn btn-ghost ${isActive('/charts') ? 'bg-blue-500 text-white' : ''}`}  // Appliquer le style actif
+              >
+                Les graphiques
+              </Link>
             </div>
           </div>
         </div>
+        
+        {/* Dropdown Avatar */}
         <div className="flex-none gap-2">
           <div className="dropdown dropdown-end">
             <div
